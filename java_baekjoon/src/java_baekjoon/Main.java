e337097d59dd33630cadbcbffa3c92bfa32c873b
EdenKwon <ybinn@caresquare.kr>
2024-01-04 20:23:52 +0900

    #ADD - Chapter2 ex01~03

--- java_baekjoon/src/java_baekjoon/Main.java
+++ java_baekjoon/src/java_baekjoon/Main.java
@@ -4,7 +4,56 @@ import java.util.*;
 
 public class Main {
 	public static void main(String[] args) {
-	
+		ex03();
 	}
 	
+	public static void ex01() {
+		int a, b;
+		Scanner sc = new Scanner(System.in);
+		
+		a = sc.nextInt();
+		b = sc.nextInt();
+		
+		if(a > b) 
+			System.out.println(">");
+		else if (a == b)
+			System.out.println("==");
+		else
+			System.out.println("<");
+	}
+	
+	public static void ex02() {
+		int a;
+		Scanner sc = new Scanner(System.in);
+		
+		a = sc.nextInt();
+		
+		if(a >= 90) 
+			System.out.println("A");
+		else if(a >= 80) 
+			System.out.println("B");
+		else if(a >= 70)
+			System.out.println("C");
+		else if(a >= 60)
+			System.out.println("D");
+		else
+			System.out.println("F");
+	}
+	
+	public static void ex03() {
+		int a;
+		Scanner sc = new Scanner(System.in);
+		
+		a = sc.nextInt();
+		
+		if(a%4 == 0) {
+			if(a%100 == 0 && a%400 != 0) {
+				System.out.println("0");
+				return;
+			}
+			System.out.println("1");
+			return;
+		}
+		System.out.println("0");
+	}
 }
