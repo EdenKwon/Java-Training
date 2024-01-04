04edcb3b129f3e85206d8a849a0c319242be9be1
EdenKwon <ybinn@caresquare.kr>
2024-01-04 17:05:31 +0900

    #ADD - ex07~09

--- java_baekjoon/src/java_baekjoon/Main.java
+++ java_baekjoon/src/java_baekjoon/Main.java
@@ -4,7 +4,7 @@ import java.util.*;
 
 public class Main {
 	public static void main(String[] args) {
-		ex06();
+		ex09();
 	}
 	
 	public static void ex01() {
@@ -64,5 +64,33 @@ public class Main {
 		System.out.println(a*b);
 		System.out.println(a/b);
 		System.out.println(a%b);
+	}
+	
+	public static void ex07() {	
+		Scanner scanner = new Scanner(System.in);
+		String s = scanner.next();
+		
+		System.out.println(s + "??!");
+	}
+	
+	public static void ex08() {	
+		Scanner scanner = new Scanner(System.in);
+		int year = scanner.nextInt();
+		
+		System.out.println(year-543);
+	}
+	
+	public static void ex09() {	
+		int a, b, c;
+		Scanner scanner = new Scanner(System.in);
+		a = scanner.nextInt();
+		b = scanner.nextInt();
+		c = scanner.nextInt();
+
+		System.out.println((a+b)%c);
+		System.out.println(((a%c) + (b%c))%c);
+		System.out.println((a*b)%c);
+		System.out.println(((a%c)*(b%c))%c);
+		
 	}
 }
