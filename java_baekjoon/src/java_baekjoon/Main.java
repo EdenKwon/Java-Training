8e0a1aac2f94d85d2e8c1e4cd9f08aec077cd2d6
EdenKwon <ybinn@caresquare.kr>
2024-01-04 16:31:55 +0900

    #ADD - ex01~03

---
+++ java_baekjoon/src/java_baekjoon/Main.java
@@ -0,0 +1,33 @@
+package java_baekjoon;
+
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+		ex03();
+	}
+	
+	public static void ex01() {
+		System.out.print("Hello World!");
+	}
+	
+	public static void ex02() {
+		int a,b;
+		Scanner scanner = new Scanner(System.in);
+		
+		a = scanner.nextInt();
+		b = scanner.nextInt();
+		
+		System.out.println(a+b);
+	}
+	
+	public static void ex03() {
+		int a,b;
+		Scanner scanner = new Scanner(System.in);
+		
+		a = scanner.nextInt();
+		b = scanner.nextInt();
+		
+		System.out.println(a-b);
+	}
+}
