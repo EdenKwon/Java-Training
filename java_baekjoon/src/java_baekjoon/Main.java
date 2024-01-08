3a50fbe554b9cd8f51c325bb8341655ab2405737
EdenKwon <ybinn@caresquare.kr>
2024-01-08 22:32:27 +0900

    #ADD - Chapter4 ex01

--- java_baekjoon/src/java_baekjoon/Main.java
+++ java_baekjoon/src/java_baekjoon/Main.java
@@ -1,14 +1,30 @@
 package java_baekjoon;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.io.*;
 import java.util.*;
 
 public class Main {
 	public static void main(String[] args) throws Exception {
+		ex01();
+	}
+	
+	public static void ex01() throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int a = Integer.parseInt(br.readLine());
 		
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		int arr[] = new int[a];
+		for (int i=0; i<a; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		int num = Integer.parseInt(br.readLine());
+		int count = 0;
+		for (int i = 0; i < a; i++) {
+			if (arr[i] == num) {
+				count++;
+			}
+		}
+		System.out.println(count);
 	}
 }
