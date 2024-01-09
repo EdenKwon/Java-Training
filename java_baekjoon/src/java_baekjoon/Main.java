078d35ba99da59461502ace7cc5dc2002f8873ed
EdenKwon <ybinn@caresquare.kr>
2024-01-09 15:31:05 +0900

    #ADD - Chapter4 ex02~ex04

--- java_baekjoon/src/java_baekjoon/Main.java
+++ java_baekjoon/src/java_baekjoon/Main.java
@@ -5,7 +5,7 @@ import java.util.*;
 
 public class Main {
 	public static void main(String[] args) throws Exception {
-		ex01();
+		ex04();
 	}
 	
 	public static void ex01() throws IOException {
@@ -26,5 +26,70 @@ public class Main {
 			}
 		}
 		System.out.println(count);
+	}
+	
+	public static void ex02() throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int total_num = Integer.parseInt(st.nextToken());
+		int value = Integer.parseInt(st.nextToken());
+		
+		int arr[] = new int[total_num];
+		st = new StringTokenizer(br.readLine());
+		for (int i=0; i<total_num; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		for (int i=0; i<total_num; i++) {
+			if (arr[i] < value) {
+				bw.write(arr[i] + " ");
+			}
+		}
+		bw.flush();
+		bw.close();
+	}
+	
+	public static void ex03() throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int total_num = Integer.parseInt(st.nextToken());
+		int max=-1000000, min=1000000;
+		int arr[] = new int[total_num];
+		st = new StringTokenizer(br.readLine());
+		for (int i=0; i<total_num; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		for(int i : arr) {
+			if (max < i) {
+				max = i;
+			}
+			if (min > i) {
+				min = i;
+			}
+		}
+		
+		bw.write(min + " " + max);
+		bw.flush();
+		bw.close();
+	}
+	
+	public static void ex04() throws IOException {
+		Scanner sc = new Scanner(System.in);
+		int arr[] = new int[9];
+		int max = 0, count = 0;
+		
+		for (int i=0; i<9; i++) {
+			arr[i] = sc.nextInt();
+			
+			if(arr[i] > max) {
+				max = arr[i];
+				count = i;
+			}
+		}
+		System.out.println(max);
+		System.out.println(count+1);
 	}
 }
