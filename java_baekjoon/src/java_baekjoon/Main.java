0c56f2a500e69d2a7b657358ab50016e1a159f4b
EdenKwon <ybinn@caresquare.kr>
2024-01-09 17:39:48 +0900

    #ADD - Chapter4 ex05~ex07

--- java_baekjoon/src/java_baekjoon/Main.java
+++ java_baekjoon/src/java_baekjoon/Main.java
@@ -5,7 +5,7 @@ import java.util.*;
 
 public class Main {
 	public static void main(String[] args) throws Exception {
-		ex04();
+		ex07();
 	}
 	
 	public static void ex01() throws IOException {
@@ -91,5 +91,85 @@ public class Main {
 		}
 		System.out.println(max);
 		System.out.println(count+1);
+	}
+	
+	public static void ex05() throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int total = Integer.parseInt(st.nextToken());
+		int count = Integer.parseInt(st.nextToken());
+		int arr[] = new int[total];
+		Arrays.fill(arr, 0);
+		
+		for (int i = 0; i < count; i++) {
+			st = new StringTokenizer(br.readLine());
+			int start = Integer.parseInt(st.nextToken()) - 1;
+			int end = Integer.parseInt(st.nextToken());
+			int value = Integer.parseInt(st.nextToken());
+			
+			Arrays.fill(arr, start, end, value);
+		}
+		
+		for (int i : arr) {
+			bw.write(i + " ");
+		}
+		
+		bw.flush();
+		bw.close();
+	}
+	
+	public static void ex06() throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int total = Integer.parseInt(st.nextToken());
+		int count = Integer.parseInt(st.nextToken());
+		int arr[] = new int[total];
+		for (int i = 0; i < total; i++) {
+			Arrays.fill(arr, i, i+1, i+1);
+		}
+		
+		for (int i = 0; i < count; i++) {
+			st = new StringTokenizer(br.readLine());
+			int basket1 = Integer.parseInt(st.nextToken());
+			int basket2 = Integer.parseInt(st.nextToken());
+			
+			int temp = 0;
+			temp = arr[basket1-1];
+			arr[basket1-1] = arr[basket2-1];
+			arr[basket2-1] = temp;
+		}
+		
+		for (int i : arr) {
+			bw.write(i + " ");
+		}
+		
+		bw.flush();
+		bw.close();
+	}
+	
+	public static void ex07() throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int arr[] = new int[30];
+		Arrays.fill(arr, 0);
+		
+		for (int i = 0; i < 28; i++) {
+			int num = Integer.parseInt(br.readLine());
+			arr[num-1] = num;
+		}
+		
+		for (int i = 0; i < 30; i++) {
+			if (arr[i] == 0) {
+				bw.write((i + 1) + "\n");
+			}
+		}
+		
+		bw.flush();
+		bw.close();
 	}
 }
