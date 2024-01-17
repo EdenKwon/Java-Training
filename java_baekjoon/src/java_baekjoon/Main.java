3597ffc39fbb1f8f6a70952d962df4870fedb5b5
EdenKwon <ybinn@caresquare.kr>
2024-01-17 15:34:25 +0900

    #ADD - Chapter5 ex01~ex03

--- java_baekjoon/src/java_baekjoon/Main.java
+++ java_baekjoon/src/java_baekjoon/Main.java
@@ -5,6 +5,35 @@ import java.util.*;
 
 public class Main {
 	public static void main(String[] args) throws Exception {
-		;
+		ex03();
+	}
+	
+	public static void ex01() throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String n = br.readLine();
+		int num = Integer.parseInt(br.readLine());
+		char word = n.charAt(num-1);
+		
+		System.out.println(word);
+	}
+	
+	public static void ex02() throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String n = br.readLine();
+		System.out.println(n.length());
+	}
+	
+	public static void ex03() throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int num = Integer.parseInt(br.readLine());
+        String arr[] = new String[num];
+        
+		for (int i = 0; i < num; i++) {
+			arr[i] = br.readLine();
+		}
+		
+		for (int i = 0; i < num; i++) {
+			System.out.println(String.valueOf(arr[i].charAt(0)) + String.valueOf(arr[i].charAt(arr[i].length()-1)));
+		}
 	}
 }
