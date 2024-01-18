1a360b940e3cbdee9d5b66f730045f0927961c44
EdenKwon <ybinn@caresquare.kr>
2024-01-18 22:03:09 +0900

    #ADD - Chapter6 ex01~ex02

--- java_baekjoon/src/java_baekjoon/Main.java
+++ java_baekjoon/src/java_baekjoon/Main.java
@@ -6,6 +6,44 @@ import java.nio.charset.StandardCharsets;
 
 public class Main {
 	public static void main(String[] args) throws Exception {
-		;
+		ex02();
+	}
+	
+	public static void ex01() throws Exception {
+		System.out.println("         ,r'\"7");
+		System.out.println("r`-_   ,'  ,/");
+		System.out.println(" \\. \". L_r'");
+		System.out.println("   `~\\/");
+		System.out.println("      |");
+		System.out.println("      |");
+	}
+	
+	
+	public static void ex02() throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int num[] = new int[6];
+		
+		for (int i = 0; i < 6; i++) {
+				num[i] = Integer.parseInt(st.nextToken());
+				switch (i) {
+					case 0:
+					case 1:
+						bw.write(1 - num[i] + " ");
+						break;
+					case 2:
+					case 3:
+					case 4:
+						bw.write(2 - num[i] + " ");
+						break;
+					case 5:
+						bw.write(8 - num[i] + " ");
+						break;
+				}
+		}
+		
+		bw.flush();
+		bw.close();
 	}
 }
