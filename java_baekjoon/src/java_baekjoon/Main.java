1053b5af4665c38f36d4f0dc3d0cd3287929985a
EdenKwon <ybinn@caresquare.kr>
2024-01-19 17:55:46 +0900

    #ADD - Chapter6 ex03~ex05

--- java_baekjoon/src/java_baekjoon/Main.java
+++ java_baekjoon/src/java_baekjoon/Main.java
@@ -6,7 +6,7 @@ import java.nio.charset.StandardCharsets;
 
 public class Main {
 	public static void main(String[] args) throws Exception {
-		ex02();
+		ex05();
 	}
 	
 	public static void ex01() throws Exception {
@@ -43,6 +43,82 @@ public class Main {
 				}
 		}
 		
+		bw.flush();
+		bw.close();
+	}
+	
+	public static void ex03() throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int num = Integer.parseInt(br.readLine());
+		
+		for (int i = 1; i <= num*2-1; i++) {
+			for (int j = 1; j <= num*2-1; j++) {
+				if(i<=num) {
+					if(j<=num-i) {
+						bw.write(" ");
+					}
+					else if(j<num+i) {
+						bw.write("*");
+					}
+				}
+				else {
+					if(j<=i-num) {
+						bw.write(" ");
+					}
+					else if(j<=num*2-1-(i-num)) {
+						bw.write("*");
+					}
+				}
+			}
+			bw.write("\n");
+		}
+		bw.flush();
+		bw.close();
+	}
+	
+	public static void ex04() throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		String word = br.readLine();
+		
+		bw.write(String.valueOf(isPalindrome(word) ? 1 : 0));
+		bw.flush();
+		bw.close();
+	}
+	
+	public static boolean isPalindrome(String word) {
+		int len = word.length();
+		for (int i = 0; i < len / 2; i++) {
+			if (word.charAt(i) != word.charAt(len - 1 - i)) {
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	public static void ex05() throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		String word = br.readLine().toUpperCase();
+
+		int Alphabet[] = new int[26];
+		for (int i = 0; i < word.length(); i++) {
+			Alphabet[word.charAt(i) - 65]++;
+		}
+		
+		int max = 0;
+		char maxAlphabet = '?';
+		for (int i = 0; i < 26; i++) {
+			if (Alphabet[i] > max) {
+				max = Alphabet[i];
+				maxAlphabet = (char) (i + 65);
+			} else if (Alphabet[i] == max) {
+				maxAlphabet = '?';
+			}
+		}
+		
+		bw.write(maxAlphabet);
 		bw.flush();
 		bw.close();
 	}
