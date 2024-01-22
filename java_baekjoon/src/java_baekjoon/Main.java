3a034e7bf19bed9290c5946eeb990d52273b9908
EdenKwon <ybinn@caresquare.kr>
2024-01-22 21:13:40 +0900

    #ADD - Chapter6 ex01~ex02

--- java_baekjoon/src/java_baekjoon/Main.java
+++ java_baekjoon/src/java_baekjoon/Main.java
@@ -6,6 +6,68 @@ import java.nio.charset.StandardCharsets;
 
 public class Main {
 	public static void main(String[] args) throws Exception {
-        ;
+        ex02();
+	}
+	
+	public static void ex01() throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int height = Integer.parseInt(st.nextToken());
+		int width = Integer.parseInt(st.nextToken());
+		int arr1[][] = new int[height][width];
+		int arr2[][] = new int[height][width];
+		
+		for (int i = 0; i < height; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < width; j++) {
+				arr1[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		
+		for (int i = 0; i < height; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < width; j++) {
+				arr2[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		
+		for (int i = 0; i < height; i++) {
+			for (int j = 0; j < width; j++) {
+				bw.write(arr1[i][j] + arr2[i][j] + " ");
+			}
+			bw.write("\n");
+		}
+		
+		bw.flush();
+		bw.close();
+	}
+	
+	public static void ex02() throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		int arr[][] = new int[9][9];
+		int max = 0;
+		int x = 0, y = 0;
+		
+		for(int i=0; i<9; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+            for(int j=0; j<9; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+                if(max < arr[i][j]) {
+                    max = arr[i][j];
+                    x = i+1;
+                    y = j+1;
+                }
+            }
+        }
+		
+		bw.write(max + "\n");
+		bw.write(x + " " + y);
+		
+		bw.flush();
+		bw.close();
 	}
 }
