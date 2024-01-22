049f6a7cfdf3cd79463060b91cdc4706d5b320a4
EdenKwon <ybinn@caresquare.kr>
2024-01-22 17:18:05 +0900

    #ADD - Chapter6 ex06~ex08

--- java_baekjoon/src/java_baekjoon/Main.java
+++ java_baekjoon/src/java_baekjoon/Main.java
@@ -6,7 +6,7 @@ import java.nio.charset.StandardCharsets;
 
 public class Main {
 	public static void main(String[] args) throws Exception {
-		ex05();
+		ex08();
 	}
 	
 	public static void ex01() throws Exception {
@@ -122,4 +122,97 @@ public class Main {
 		bw.flush();
 		bw.close();
 	}
+	
+	public static void ex06() throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		String[] croatia = { "c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z=" };
+		String word = br.readLine();
+		
+		int count = 0;
+		while (count < croatia.length) {
+			if (word.contains(croatia[count])) {
+				word = word.replace(croatia[count], " ");
+			}
+			count++;
+		}
+		bw.write(String.valueOf(word.length()));
+		bw.flush();
+		bw.close();
+	}
+	
+	public static void ex07() throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int num = Integer.parseInt(br.readLine());
+		int count = 0;
+		
+		for (int i = 0; i < num; i++) {
+			String word = br.readLine();
+			if (isGroupWord(word)) {
+				count++;
+			}
+		}
+		
+		bw.write(String.valueOf(count));
+		bw.flush();
+		bw.close();
+	}
+	
+	public static boolean isGroupWord(String word) {
+		int len = word.length();
+		for (int i = 0; i < len - 1; i++) {
+			if (word.charAt(i) != word.charAt(i + 1)) {
+				for (int j = i + 2; j < len; j++) {
+					if (word.charAt(i) == word.charAt(j)) {
+						return false;
+					}
+				}
+			}
+		}
+		return true;
+	}
+	
+	public static void ex08() throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		double subSum = 0;
+		double gradeSum = 0;
+		
+		for (int i=0; i<20; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			String subject = st.nextToken();
+			double score = Double.parseDouble(st.nextToken());
+			String grade = st.nextToken();
+			
+			if (grade.equals("P")) {
+				continue;
+			}
+			
+			subSum += score;
+			gradeSum += score * convertGrade(grade);	
+		}
+		
+		bw.write(String.valueOf(gradeSum / subSum));
+		bw.flush();
+		bw.close();
+	}
+	
+	private static final Map<String, Double> gradeToGPA = new HashMap<>();
+
+    static {
+        gradeToGPA.put("A+", 4.5);
+        gradeToGPA.put("A0", 4.0);        
+        gradeToGPA.put("B+", 3.5);
+        gradeToGPA.put("B0", 3.0);
+        gradeToGPA.put("C+", 2.5);
+        gradeToGPA.put("C0", 2.0);
+        gradeToGPA.put("D+", 1.5);
+        gradeToGPA.put("D0", 1.0);
+        gradeToGPA.put("F", 0.0);
+    }
+
+    public static double convertGrade(String Grade) {
+        return gradeToGPA.getOrDefault(Grade, 0.0);
+    }
 }
