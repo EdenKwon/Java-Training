2a03db7974d80d1308378a273b269b2cfee71e43
EdenKwon <ybinn@caresquare.kr>
2024-01-24 16:30:20 +0900

    #ADD - Chapter8 ex03~ex05

--- java_baekjoon/src/java_baekjoon/Main.java
+++ java_baekjoon/src/java_baekjoon/Main.java
@@ -6,7 +6,7 @@ import java.nio.charset.StandardCharsets;
 
 public class Main {
 	public static void main(String[] args) throws Exception {
-        ex02();
+        ex05();
 	}
 	
 	public static void ex01() throws Exception {
@@ -72,5 +72,62 @@ public class Main {
         
         bw.flush();
         bw.close();
+	}
+	
+	public static void ex03() throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int Quater = 0, Dime = 0, Nickel = 0, Penny = 0;
+		int testCase = Integer.parseInt(br.readLine());
+		for (int i = 0; i < testCase; i++) {
+			int cent = Integer.parseInt(br.readLine());
+			Quater = cent / 25;
+			cent %= 25;
+			Dime = cent / 10;
+			cent %= 10;
+			Nickel = cent / 5;
+			cent %= 5;
+			Penny = cent;
+
+			bw.write(Quater + " " + Dime + " " + Nickel + " " + Penny + "\n");
+		}
+		
+		bw.flush();
+		bw.close();
+	}
+	
+	public static void ex04() throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int num = Integer.parseInt(br.readLine());
+
+		num = (int) Math.pow(getPoint(num), 2);
+		bw.write(num + "");
+		bw.flush();
+		bw.close();
+	}
+	
+	public static int getPoint(int num) {
+		if (num == 1)
+			return 3;
+		else
+			return getPoint(num - 1) + getPoint(num - 1) - 1;
+	}
+	
+	public static void ex05() throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int roomNum = Integer.parseInt(br.readLine());
+		int count = 1;
+		int i = 1;
+		
+		while(i < roomNum) {
+            i += 6 * count;
+            count ++;
+        }
+		
+		bw.write(count + "");
+		bw.flush();
+		bw.close();
 	}
 }
