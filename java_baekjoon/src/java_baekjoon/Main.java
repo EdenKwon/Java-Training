6d62fa4fbc625abc0dcadd08bb77ff66c9883281
EdenKwon <ybinn@caresquare.kr>
2024-01-26 15:39:25 +0900

    #ADD - Chapter11 ex01~ex03

--- java_baekjoon/src/java_baekjoon/Main.java
+++ java_baekjoon/src/java_baekjoon/Main.java
@@ -6,6 +6,40 @@ import java.nio.charset.StandardCharsets;
 
 public class Main {
 	public static void main(String[] args) throws Exception {
-		;
+		ex03();
+	}
+	
+	public static void ex01() throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        bw.write(1 + "\n" + 0);
+        
+        bw.flush();
+        bw.close();
+	}
+	
+	public static void ex02() throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+        int num = Integer.parseInt(br.readLine());
+        
+		bw.write(num + "\n" + 1);
+		
+		bw.flush();
+        bw.close();
+	}
+	
+	public static void ex03() throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		int num = Integer.parseInt(br.readLine());
+
+		bw.write((int)Math.pow(num, 2) + "\n" + 2);
+
+		bw.flush();
+		bw.close();
 	}
 }
