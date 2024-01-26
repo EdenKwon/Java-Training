5f536e3fc052bfaf064881555ba31e5887644f8e
EdenKwon <ybinn@caresquare.kr>
2024-01-26 17:29:28 +0900

    #ADD - Chapter11 ex04~ex07

--- java_baekjoon/src/java_baekjoon/Main.java
+++ java_baekjoon/src/java_baekjoon/Main.java
@@ -6,7 +6,7 @@ import java.nio.charset.StandardCharsets;
 
 public class Main {
 	public static void main(String[] args) throws Exception {
-		ex03();
+		ex07();
 	}
 	
 	public static void ex01() throws Exception {
@@ -37,9 +37,67 @@ public class Main {
 
 		int num = Integer.parseInt(br.readLine());
 
-		bw.write((int)Math.pow(num, 2) + "\n" + 2);
+		bw.write((long)Math.pow(num, 2) + "\n" + 2);
 
 		bw.flush();
 		bw.close();
+	}
+	
+	public static void ex04() throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		long num = Integer.parseInt(br.readLine());
+
+		bw.write(num*(num-1)/2 + "\n" + 2);
+
+		bw.flush();
+		bw.close();
+	}
+	
+	public static void ex05() throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		long num = Integer.parseInt(br.readLine());
+
+		bw.write((long)(num * num * num) + "\n" + 3);
+
+		bw.flush();
+		bw.close();
+	}
+	
+	public static void ex06() throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		long num = Integer.parseInt(br.readLine());
+
+		bw.write((long) (num * (num-1) * (num-2))/6 + "\n" + 3);
+
+		bw.flush();
+		bw.close();
+	}
+	
+	public static void ex07() throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int a1 = Integer.parseInt(st.nextToken());
+        int a0 = Integer.parseInt(st.nextToken());
+
+        int c = Integer.parseInt(br.readLine());
+        int n0 = Integer.parseInt(br.readLine());
+        br.close();
+
+        if (a1*n0 + a0 <= c*n0 && c >= a1) {
+            bw.write("1");
+        } else {
+            bw.write("0");
+        }
+
+        bw.flush();
+        bw.close();
 	}
 }
